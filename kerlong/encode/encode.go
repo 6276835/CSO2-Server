3e1832e10e8163c3c4db0ddc5fbc030ae0382dc1
6276835 <6276835@qq.com>
2024-03-14 17:31:40 +0800

    commit message

--- kerlong/encode/encode.go
+++ kerlong/encode/encode.go
@@ -2,58 +2,41 @@ package encode
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io"
 
-	iconv "github.com/djimenez/iconv-go"
+	"github.com/djimenez/iconv-go"
 	"golang.org/x/text/encoding/simplifiedchinese"
 	"golang.org/x/text/transform"
 )
 
-var (
-	CVtolocal *iconv.Converter
-	CVtoutf8  *iconv.Converter
-)
-
-func InitConverter(local string) bool {
-	cv, err := iconv.NewConverter("utf-8", local)
-	if err != nil {
-		panic(err)
-	}
-	CVtolocal = cv
-	cv, err = iconv.NewConverter(local, "utf-8")
-	if err != nil {
-		panic(err)
-	}
-	CVtoutf8 = cv
-	return true
-}
-
-//GbkToUtf8 转换GBK编码到UTF-8编码
+// GbkToUtf8 转换GBK编码到UTF-8编码
 func GbkToUtf8(str []byte) (b []byte, err error) {
 	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewDecoder())
-	b, err = ioutil.ReadAll(r)
+	b, err = io.ReadAll(r)
 	if err != nil {
 		return
 	}
 	return
 }
 
-//Utf8ToGbk 转换UTF-8编码到GBK编码
+// Utf8ToGbk 转换UTF-8编码到GBK编码
 func Utf8ToGbk(str []byte) (b []byte, err error) {
 	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewEncoder())
-	b, err = ioutil.ReadAll(r)
+	b, err = io.ReadAll(r)
 	if err != nil {
 		return
 	}
 	return
 }
 
+// Utf8ToLocal 转换UTF-8编码到本地编码
 func Utf8ToLocal(str string) (b string, err error) {
-	buf, err := CVtolocal.ConvertString(str)
+	buf, err := iconv.ConvertString(str, "UTF-8", "GBK")
 	return string(buf), err
 }
 
+// LocalToUtf8 转换本地编码到UTF-8编码
 func LocalToUtf8(str string) (b string, err error) {
-	buf, err := CVtoutf8.ConvertString(str)
+	buf, err := iconv.ConvertString(str, "GBK", "UTF-8")
 	return string(buf), err
 }
